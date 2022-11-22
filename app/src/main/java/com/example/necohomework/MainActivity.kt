2fd1747bae68d1107b53456065934a87efe4c2be
Mansur <mahcypc@mail.ru>
2022-11-23 01:17:34 +0300

    Added comments

--- app/src/main/java/com/example/necohomework/MainActivity.kt
+++ app/src/main/java/com/example/necohomework/MainActivity.kt
@@ -8,17 +8,20 @@ import com.example.necohomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    //    Создаем переменную для LifeCycle модели
     private val dataModel: DataModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    //  Заполняем FrameLayout фрагмаентами
         openFrag(FragmentTop.newInstance(), R.id.fragTop)
         openFrag(FragmentBottom.newInstance(), R.id.fragBottom)
+        // Добавляем LifeCycle слушатель для текстового поля
         dataModel.messageForActivity.observe(this) { binding.tvMessageActivity.text = it }
     }
-
+//  Функция для заполнения FrameLayout фрагмаентами
     private fun openFrag(f: Fragment, idHolder: Int){
         supportFragmentManager.beginTransaction().replace(idHolder, f).commit()
     }
