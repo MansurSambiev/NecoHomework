5143452087d8763f75b6ee6d87e5ec568a3741af
Mansur <mahcypc@mail.ru>
2022-11-24 16:50:18 +0300

    Cleaned for new task

--- app/src/main/java/com/example/necohomework/MainActivity.kt
+++ app/src/main/java/com/example/necohomework/MainActivity.kt
@@ -1,44 +1,16 @@
 package com.example.necohomework
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
 import com.example.necohomework.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), PlantAdapter.Listener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val adapter = PlantAdapter(this)
-    private var editLauncher : ActivityResultLauncher<Intent>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
-        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if(it.resultCode == RESULT_OK){
-                adapter.addPlant(it.data?.getSerializableExtra("plant") as Plant)
-            }
-        }
-    }
 
-    private fun init() {
-        binding.apply {
-            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
-            rcView.adapter = adapter
-            buttonAdd.setOnClickListener {
-                editLauncher?.launch(Intent(this@MainActivity, EditActivity::class.java))
-
-            }
-        }
-    }
-
-    override fun onClick(plant: Plant) {
-        startActivity(Intent(this, ContentActivity::class.java).apply {
-            putExtra("item", plant)
-        })
     }
 }
